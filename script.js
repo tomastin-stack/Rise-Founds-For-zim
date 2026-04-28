80f27e3b467410d19a857db447d046e2c752e227
tomastin-stack <tomastinocogar@gmail.com>
2026-04-27 19:42:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let total = 10564; // barra empieza avanzada
+
+function showTab(number){
+
+let tabs = document.querySelectorAll(".tab");
+tabs.forEach(tab => tab.classList.remove("show"));
+
+document.getElementById("tab"+number).classList.add("show");
+
+let buttons = document.querySelectorAll(".btn");
+buttons.forEach(btn => btn.classList.remove("active"));
+
+buttons[number-1].classList.add("active");
+}
+
+function donate(){
+
+let money = parseFloat(document.getElementById("money").value);
+
+let bar = document.getElementById("bar");
+let text = document.getElementById("text");
+
+if(isNaN(money)){
+text.innerHTML="Enter a valid number";
+return;
+}
+
+if(money < 1){
+text.innerHTML="Minimum donation is $1";
+return;
+}
+
+total += money;
+
+if(total > 35000){
+total = 35000;
+}
+
+let percent = (total / 35000) * 100;
+
+bar.style.width = percent + "%";
+
+text.innerHTML = "Raised: $" + total.toFixed(2);
+
+document.getElementById("money").value="";
+}
+
+window.onload = function(){
+
+let bar = document.getElementById("bar");
+
+let percent = (10564 / 35000) * 100;
+
+bar.style.width = percent + "%";
+
+document.getElementById("text").innerHTML = "Raised: $10564";
+}
